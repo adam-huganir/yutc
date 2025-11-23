1e74379a08a0337960e703d9b465e9e50faf56ff
Adam Huganir <adam@huganir.com>
2025-11-23 17:17:04 -0500

    revert version string change

--- pkg/version.go
+++ pkg/version.go
@@ -6,7 +6,7 @@ var version = "v0.4.0"
 
 // PrintVersion prints the current version to stdout.
 func PrintVersion() {
-	fmt.Println("yutc " + version)
+	fmt.Println(version)
 }
 
 // GetVersion returns the current version string.
